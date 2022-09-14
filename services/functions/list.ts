de05d011bb9c497670c99c3dbe5675fde76df775
Bruno Barreto Diniz <97979605+brunobdiniz@users.noreply.github.com>
2022-09-14 17:55:18 +0200

    Update list.ts
    Updated for better JSON reading.

--- services/functions/list.ts
+++ services/functions/list.ts
@@ -5,7 +5,7 @@ import notes from "../notes";
 export async function handler() {
   return {
     statusCode: 200,
-    body: JSON.stringify(notes),
+    body: JSON.stringify(notes, null, "  "),
   };
 }
 
