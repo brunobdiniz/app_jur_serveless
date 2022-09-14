0dd77014fb0b4cd1a01eb9876ca693826f4a8df8
Bruno Barreto Diniz <97979605+brunobdiniz@users.noreply.github.com>
2022-09-14 17:50:48 +0200

    Create update.ts
    Content updating after it is checked, as in Step 6.

---
+++ services/functions/update.ts
@@ -0,0 +1,24 @@
+// after checking note with id, content is updated and returned //
+
+import notes from "../notes";
+import { APIGatewayProxyHandlerV2 } from "aws-lambda";
+
+export const handler: APIGatewayProxyHandlerV2 = async (event) => {
+  const note = notes[event.pathParameters.id];
+
+  if (!note) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ error: true }),
+    };
+  }
+
+  const data = JSON.parse(event.body);
+
+  note.content = data.content;
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(note),
+  };
+};
